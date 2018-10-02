f6ba1e46efcf26f924e4a7002302bad6f6276af5
Nimish Singh <nimishs@thoughtworks.com>
2018-10-02 22:00:22 +0530

    Initial state in BurgerBuilder

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,20 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  return(
-      <div className={classes.Burger}>
-        <BurgerIngredient type="bread-top"/>
-        <BurgerIngredient type="cheese"/>
-        <BurgerIngredient type="meat"/>
-        <BurgerIngredient type="bread-bottom"/>
-      </div>
-  );
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igkey => {
+            return [...Array(props.ingredients[igkey])].map((_, i) => {
+                return <BurgerIngredient key={igkey + i} type={igkey}/>
+            });
+        });
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top"/>
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom"/>
+        </div>
+    );
 };
 
 export default burger;
