cf732010680c1f8e42cb5663997876fab2328d0d
Nimish Singh <nimishs@thoughtworks.com>
2018-12-30 01:16:00 +0530

    Navigate to Checkout summary on clicking Continue

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -95,25 +95,26 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        this.setState({loading: true});
-
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Customer 1',
-                address: {
-                    street: 'Street 1',
-                    zipcode: '1',
-                    country: 'Country 1'
-                },
-                email: '1@email.com'
-            },
-            deliveryMethod: 'fastest'
-        };
-        axios.post('/orders.json', order)
-            .then(response => this.setState({loading: false, purchasing: false}))
-            .catch(error => this.setState({loading: false, purchasing: false}));
+        // this.setState({loading: true});
+        //
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Customer 1',
+        //         address: {
+        //             street: 'Street 1',
+        //             zipcode: '1',
+        //             country: 'Country 1'
+        //         },
+        //         email: '1@email.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // };
+        // axios.post('/orders.json', order)
+        //     .then(response => this.setState({loading: false, purchasing: false}))
+        //     .catch(error => this.setState({loading: false, purchasing: false}));
+        this.props.history.push('/checkout');
     };
 
     render() {
