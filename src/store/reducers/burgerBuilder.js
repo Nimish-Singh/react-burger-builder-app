bd317df28b99bd4f93a70ea1699091d183e87d94
Nimish Singh <nimishs@thoughtworks.com>
2019-02-02 18:54:09 +0530

    Refactor reducers to use utility method to update objects

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionsTypes from '../actions/actionTypes';
+import {updateObject} from '../utility';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -16,43 +17,38 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionsTypes.ADD_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-                },
+            const updatedIngredient = {[action.ingredientName]: state.ingredients[action.ingredientName] + 1};
+            const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+            const updatedState = {
+                ingredients: updatedIngredients,
                 totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
             };
+            return updateObject(state, updatedState);
 
         case actionsTypes.REMOVE_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-                },
+            const updatedIngredientR = {[action.ingredientName]: state.ingredients[action.ingredientName] - 1};
+            const updatedIngredientsR = updateObject(state.ingredients, updatedIngredientR);
+            const updatedStateR = {
+                ingredients: updatedIngredientsR,
                 totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
             };
+            return updateObject(state, updatedStateR);
+
 
         case actionsTypes.SET_INGREDIENTS:
-            return {
-                ...state,
+            return updateObject(state, {
                 ingredients: {
                     salad: action.ingredients.salad,
                     bacon: action.ingredients.bacon,
                     cheese: action.ingredients.cheese,
                     meat: action.ingredients.meat,
-                    totalPrice: 4
                 },
+                totalPrice: 4,
                 error: false
-            };
+            });
 
         case actionsTypes.FETCH_INGREDIENTS_FAILED:
-            return {
-                ...state,
-                error: true
-            };
+            return updateObject(state, {error: true});
 
         default:
             return state;
