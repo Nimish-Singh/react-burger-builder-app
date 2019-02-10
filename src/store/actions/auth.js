f02dae25d8ac419cc8d986e1ca9b03c638b67cd3
Nimish Singh <nimishs@thoughtworks.com>
2019-02-10 17:29:05 +0530

    Provide Sign-in switch to user

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -21,7 +21,7 @@ export const authFail = error => {
     }
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
@@ -29,7 +29,11 @@ export const auth = (email, password) => {
             password,
             returnSecureToken: true
         };
-        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDqVxhhEizJFr9MONr-GejIwI50xRgxZm0', authData)
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDqVxhhEizJFr9MONr-GejIwI50xRgxZm0';
+        if (!isSignup) {
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDqVxhhEizJFr9MONr-GejIwI50xRgxZm0';
+        }
+        axios.post(url, authData)
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data));
