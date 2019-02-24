d378334dc772f10e8e04e9e1b6609b34dd6960b4
Nimish Singh <nimishs@thoughtworks.com>
2019-02-24 16:00:19 +0530

    Add test to assert exact child component

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -23,4 +23,9 @@ describe('<NavigationItems />', () => {
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 
+    it('should render <NavigationItem /> element with logout if authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+    });
+
 });
